4f6c7151b27a8ebbebbc0d8cd89ed7a006399ed2
Daniel Thomas <daniel.thomas@dpool.com>
2021-04-09 11:15:01 +0200

    *

--- Job/Script/job.deploy.script.bash
+++ Job/Script/job.deploy.script.bash
@@ -1,7 +1,7 @@
 eval $(ssh-agent -s)
 wget https://raw.githubusercontent.com/dpool/devops/main/Job/Excludes/Deploy/typo3.txt
 ssh-add <(echo "$SSH_PRIVATE_KEY")
-rsync -rlp -e "ssh -o StrictHostKeyChecking=no -p $SSH_PORT" --delete --exclude-from=./typo3.txt --exclude-from=./local-deploy-rsync-excludes.txt --exclude=/Tests --exclude=auth.json --exclude=local-deploy-rsync-excludes.txt --exclude=codeception.yml .Build/ $SSH_LOGIN:$REMOTE_PATH
+rsync -rlp -e "ssh -o StrictHostKeyChecking=no -p $SSH_PORT" --delete --exclude-from=./typo3.txt --exclude-from=./local-deploy-rsync-excludes.txt --exclude=/Tests --exclude=codeception.yml .Build/ $SSH_LOGIN:$REMOTE_PATH
 ssh -o StrictHostKeyChecking=no -p $SSH_PORT $SSH_LOGIN "
   php "$REMOTE_PATH"vendor/bin/typo3cms database:export | gzip > "$REMOTE_PATH"backups/databases/beforedeploy.gz
   php "$REMOTE_PATH"vendor/bin/typo3cms cache:flush --files-only
